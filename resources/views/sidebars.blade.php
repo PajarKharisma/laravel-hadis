bb10d0f10fb663c58559dc459129b0c42d94fabf
PajarKharisma <pajarkharisma@hotmail.com>
2020-03-05 19:47:56 +0700

    update

--- resources/views/sidebars.blade.php
+++ resources/views/sidebars.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-light border-right" id="sidebar-wrapper">
     <div class="sidebar-heading">Aplikasi Hadits </div>
     <div class="list-group list-group-flush">
-        <a href="/" class="list-group-item list-group-item-action bg-light">Beranda</a>
+        <a href=" {{ url('hadits') }} " class="list-group-item list-group-item-action bg-light">Beranda</a>
         <a href="{{ url('hadits/pencarian') }}" class="list-group-item list-group-item-action bg-light">Halaman Pencarian</a>
         <a href="{{ url('hadits/tentang') }}" class="list-group-item list-group-item-action bg-light">Tentang</a>
         <a href="{{ url('hadits/bantuan') }}" class="list-group-item list-group-item-action bg-light">Bantuan</a>
